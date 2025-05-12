da04f47a22a08c22c00138e83e3d51a750a07ee9
unknown <genesisdanielamartinezcardenas@gmail.com>
2025-05-12 14:34:55 -0700

    nuevo

--- src/AnimalsShow.jsx
+++ src/AnimalsShow.jsx
@@ -5,7 +5,7 @@ import dog from './assets/svg/dog.svg'
 import cat from './assets/svg/cat.svg'
 import cow from './assets/svg/cow.svg'
 import gator from './assets/svg/gator.svg'
-import heart from ' ./assets/svg/heart.svg'
+import heart from './assets/svg/heart.svg'
 import horse from './assets/svg/horse.svg'
 
 import './AnimalsShow.css'
